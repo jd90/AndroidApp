4318a6a8194bf98c83c2eb980edee694439f05a5
jd90 <1418146@rgu.ac.uk>
2016-04-23 18:28:53 +0100

    23042016 push

---
+++ ParseStarterProject/src/main/java/com/parse/starter/ClasspastTotalItem.java
@@ -0,0 +1,21 @@
+package com.parse.starter;
+
+/**
+ * Created by Borris on 23/04/2016.
+ */
+public class ClasspastTotalItem {
+
+    int percent;
+    String date;
+
+    public ClasspastTotalItem(int percent, String date){
+
+        this.percent=percent;
+        this.date=date;
+
+    }
+
+
+
+
+}
